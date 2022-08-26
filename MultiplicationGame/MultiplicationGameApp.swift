7a3a55012266e586d80d613c866592d44432723a
sedatcakir26 <sedatcakir@gmail.com>
2022-08-26 21:50:29 +0300

    Initial Commit

---
+++ MultiplicationGame/MultiplicationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationGameApp.swift
+//  MultiplicationGame
+//
+//  Created by Sedat Çakır on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
